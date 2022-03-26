7465e82fac7d6a1e20eb9a6197e1d3fbe3401d05
aishwaryaramesh1999 <r.aishwarya@thoughtworks.com>
2022-03-26 10:13:44 +0530

    Add product price as constructor parameter

--- src/main/java/Product.java
+++ src/main/java/Product.java
@@ -1,12 +1,18 @@
 public class Product {
     private String productName;
+    private Double productPrice;
 
-    public Product(String productName) {
+    public Product(String productName, Double productPrice) {
+
         this.productName = productName;
+        this.productPrice = productPrice;
     }
 
     public String getProductName() {
         return productName;
     }
 
+    public Double getProductPrice() {
+        return productPrice;
+    }
 }
